008c6d61facc545dda7123e954331677e73cb76a
Val <alu0101485287@ull.edu.es>
2022-11-03 00:01:18 +0000

    Updated bash project

--- bash_project/filesysteminfo.sh
+++ bash_project/filesysteminfo.sh
@@ -2,14 +2,16 @@
 
 # filesysteminfo -  Bash Project script
 
-## Constantes
-  TITLE=""
+## Constants
+  TITLE="-- System Archives Information --"
+  RIGHT_NOW=$(date +"%x %r%Z")
+  TIME_STAMP="Actualizada el $RIGHT_NOW por $USER"
 
-## Estilos
+## Styles
   BOLD_TEXT=$(tput bold)
   ULINE_TEXT=$(tput smul)
 
-## Colores
+## Colors
   RED_TEXT=$(tput setaf 1)
   GREEN_TEXT=$(tput setaf 2)
   YELLOW_TEXT=$(tput setaf 3)
@@ -19,10 +21,60 @@
 ## Reset Text
   RESET_TEXT=$(tput sgr0)
 
-## Funciones
+## Varibles
 
-SystemInfo() {
-  echo "${ULINE_TEXT}${BOLD_TEXT}${MAGENTA_TEXT}Sistemas de archivos${RESET_TEXT}"
-  
+
+## Functions
+
+# Usage: Function that shows how to use the program.
+
+function Usage() {
+  echo "${BOLD_TEXT}${MAGENTA_TEXT}$TITLE${RESET_TEXT}"
+  echo "${BOLD_TEXT}Script that displays mounted files system information${RESET_TEXT}"
+  echo "${BOLD_TEXT}${GREEN_TEXT}How to use:${RESET_TEXT} ./filesysteminfo [parameters]"
+  echo "${BOLD_TEXT}${ULINE_TEXT}${BLUE_TEXT}Parameter:${RESET_TEXT}"
+  echo "-h | --help : Shows how to use the program."
+  echo "-inv : Inverts the output command."
 }
 
+# SystemInfo: Function that shows the types of file systems that are mounted on the system
+
+SystemInfo() {
+  if [ $# == 0 ]; then
+    #echo ${BOLD_TEXT}"File" ${RESET_TEXT}
+    for device in $(mount | cut -d ' ' -f 5 | sort | uniq); do
+      df -aT | grep -w $device | sort -k4 -n | head -n +1 | awk '{print $1"\t" $2"\t" $4"\t" $7"\t"}'
+    done | column -t
+  
+  elif [ $1 == "-inv" ]; then
+    for device in $(mount | cut -d ' ' -f 5 | sort -r | uniq); do
+      df -aT | grep -w $device | sort -k4 -nr | head -n +1 | awk '{print $1"\t" $2"\t" $4"\t" $7"\t"'
+    done | column -t
+  fi
+}
+
+# MAIN PROGRAM
+while [ "$1" != "" ]; do
+  case "$1" in
+    -h | --help)
+      Usage
+      exit 0
+      ;;
+    -inv)
+      SystemInfo $1
+      exit 0
+      ;;
+    *)
+    cat << _EOF_
+      ${BOLD_TEXT}${RED_TEXT}ERROR: Invalid option! :(${RESET_TEXT}
+_EOF_
+    Usage
+    exit 1
+    ;;
+  esac
+  shift
+done
+
+SystemInfo
+
+exit 0
