218dfab1ab12b7c8ae10e7e8c64e208051a4092e
ValBoschP <alu0101485287@ull.edu.es>
2022-11-28 11:18:19 +0000

    -devicefile added

--- bash_project/filesysteminfo.sh
+++ bash_project/filesysteminfo.sh
@@ -3,7 +3,7 @@
 # filesysteminfo -  Bash Project script
 
 ## Constants
-  TITLE="-- System Archives Information --"
+  TITLE="-- System Files Information --"
   RIGHT_NOW=$(date +"%x %r%Z")
   TIME_STAMP="Actualizada el $RIGHT_NOW por $USER"
 
@@ -35,52 +35,63 @@ function Usage() {
   echo "-df | --devicefiles : Devices of the OS ad files"
 }
 
+error_exit() {
+  echo "${BOLD_TEXT}${RED_TEXT}ERROR: Something went wrong${RESET_TEXT}"
+  exit 1
+}
 # SystemInfo: Function that shows the types of file systems that are mounted on the system
-
 SystemInfo() {
   SORT_PARAMS=""          # Declaro una variable para ordenar la tabla al final
   if [ $invert ]; then    
     SORT_PARAMS="r"       # Si invert se cumple pues pondrá r (para inversa) en la tabla final
   fi
-  #if [ $devfiles ]; then
-  #else
-  TABLE="$(df -aT 2> /dev/null | tail -n+2 | sort -k4,2 | awk '{ print $1, $2, $4, $7 }')"
-  FINAL_TABLE=""
-  # Declaramos una variable para guardar los tipos anteriores
-  PREVIOUS_TYPE=""
-  USAGE_SUM=0
-  OLD_IFS="${IFS}"
-  IFS=$'\n'
-  for line in $TABLE; do
-    # Lee cada una de las líneas en la tabla para poder calcular todo lo que necesitamos
-    IFS=$' ' read -a LINE <<< "${line}"
-    if [[ "${PREVIOUS_TYPE}" != "${LINE[1]}" ]]; then
-      if [[ "${PREVIOUS_TYPE}" ]]; then
-        FINAL_TABLE="${FINAL_TABLE} ${FS_NAME} ${FS_TYPE} ${COUNT} ${USAGE_SUM} ${FS_HIGH} ${FS_LOW} ${FS_MOUNT}\n"
+    TABLE="$(df -aT 2> /dev/null | tail -n+2 | sort -k4,2 | awk '{ print $1, $2, $4, $7 }')"
+    FINAL_TABLE=""
+    # Declaramos una variable para guardar los tipos anteriores
+    PREVIOUS_TYPE=""
+    USAGE_SUM=0
+    OLD_IFS="${IFS}"
+    IFS=$'\n'
+    for line in $TABLE; do
+      # Lee cada una de las líneas en la tabla para poder calcular todo lo que necesitamos
+      IFS=$' ' read -a LINE <<< "${line}"
+      if [[ "${PREVIOUS_TYPE}" != "${LINE[1]}" ]]; then
+        if [[ "${PREVIOUS_TYPE}" ]]; then
+          if [[ ${devfiles} ]]; then
+            FINAL_TABLE="${FINAL_TABLE} ${FS_NAME} ${FS_TYPE} ${COUNT} ${OPENED} ${USAGE_SUM} ${FS_HIGH} ${FS_LOW} ${FS_MOUNT}\n"
+          else
+            FINAL_TABLE="${FINAL_TABLE} ${FS_NAME} ${FS_TYPE} ${COUNT} ${USAGE_SUM} ${FS_HIGH} ${FS_LOW} ${FS_MOUNT}\n"
+          fi
+        fi
+      COUNT=1
+      PREVIOUS_TYPE="${LINE[1]}"
+      USAGE_SUM="${LINE[2]}"
+      # Todas las variables de cada columna
+      FS_NAME="${LINE[0]}"
+      FS_TYPE="${LINE[1]}"
+      FS_MOUNT="${LINE[3]}"
+      OPENED=$(lsof "${FS_NAME}" 2> /dev/null | wc -l)
+      FS_HIGH=$(stat ${FS_NAME} -c %t 2> /dev/null | cut -d" " -f5 | tr -d "," | tr -d "\n")
+      FS_LOW=$(stat ${FS_NAME} -c %T 2> /dev/null | cut -d" " -f6)
+      # Si no se encuentra el valor mayor o menor, ponemos *
+      if [[ ! "${FS_HIGH}" ]]; then
+        FS_HIGH="*"
+        FS_LOW="*"
       fi
-    COUNT=1
-    PREVIOUS_TYPE="${LINE[1]}"
-    USAGE_SUM="${LINE[2]}"
-    # Todas las variables de cada columna
-    FS_NAME="${LINE[0]}"
-    FS_TYPE="${LINE[1]}"
-    FS_MOUNT="${LINE[3]}"
-    FS_HIGH=$(stat ${FS_NAME} -c %t 2> /dev/null | cut -d" " -f5 | tr -d "," | tr -d "\n")
-    FS_LOW=$(stat ${FS_NAME} -c %T 2> /dev/null | cut -d" " -f6)
-    # Si no se encuentra el valor mayor o menor, ponemos *
-    if [[ ! "${FS_HIGH}" ]]; then
-      FS_HIGH="*"
-      FS_LOW="*"
+      else
+        COUNT=$((${COUNT} + 1))
+        USAGE_SUM=$((${USAGE_SUM} + ${LINE[2]}))
+      fi
+    done
+    IFS="${OLD_IFS}"
+    FINAL_TABLE=$(echo -e "${FINAL_TABLE}" | sort -k1 -${SORT_PARAMS})
+    HEADING="NAME TYPE COUNT USAGE HIGH LOW MOUNT\n"
+    if [[ ${devfiles} ]]; then
+      HEADING="NAME TYPE COUNT OPENED USAGE HIGH LOW MOUNT\n"
     fi
-    else
-      COUNT=$((${COUNT} + 1))
-      USAGE_SUM=$((${USAGE_SUM} + ${LINE[2]}))
-    fi
-  done
-  IFS="${OLD_IFS}"
-  FINAL_TABLE=$(echo -e "${FINAL_TABLE}" | sort -k1 -${SORT_PARAMS})
-  # Imprimimos nuestra tabla final resultante
-  echo -e "${BOLD_TEXT}${ULINE_TEXT}${MAGENTA_TEXT}NAME TYPE COUNT USAGE HIGH LOW MOUNT\n"${RESET_TEXT} "${FINAL_TABLE}" | column -t
+    # Imprimimos nuestra tabla final resultante
+    echo "${RIGHT_NOW}"
+    echo -e "${BOLD_TEXT}${ULINE_TEXT}${MAGENTA_TEXT}${HEADING}${RESET_TEXT}" "${FINAL_TABLE}" | column -t
 }
 
 # MAIN PROGRAM
@@ -93,9 +104,9 @@ while [ "$1" != "" ]; do
     --invert | -inv)
       invert=1
       ;;
-    #--devicefiles | -df)
-    #  devfiles=1
-    #  ;;
+    --devicefiles | -df)
+      devfiles=1
+      ;;
     *)
     cat << _EOF_
       ${BOLD_TEXT}${RED_TEXT}ERROR: Invalid option! :(${RESET_TEXT}
