e3e9504d408cf1da1b1064fb44371a56393f2c4c
Valeria <alu0101485287@ull.edu.es>
2022-12-13 08:56:29 +0100

    terminado?

---
+++ build/src/CMakeFiles/copyfile.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "../bin/copyfile"
+  "../bin/copyfile.pdb"
+  "CMakeFiles/copyfile.dir/copyfile.cc.o"
+  "CMakeFiles/copyfile.dir/copyfile.cc.o.d"
+  "CMakeFiles/copyfile.dir/main.cc.o"
+  "CMakeFiles/copyfile.dir/main.cc.o.d"
+  "CMakeFiles/copyfile.dir/usages.cc.o"
+  "CMakeFiles/copyfile.dir/usages.cc.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/copyfile.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
